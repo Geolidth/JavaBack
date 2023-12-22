092eb287cf02c5e127d5fe04c60d1e5739a0d494
Geolidth <myyouz74@gmail.com>
2023-12-22 13:25:27 +0100

    /todo endpoint

--- todoapp/src/main/java/com/todoapp/controllers/ToDoController.java
+++ todoapp/src/main/java/com/todoapp/controllers/ToDoController.java
@@ -1,12 +1,17 @@
 package com.todoapp.controllers;
 
+import com.todoapp.models.Todo;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class ToDoController {
-    @GetMapping("/hello")
-    public String hello(){
-        return "Helló!";
+    @GetMapping("/todo")
+    public ResponseEntity<Todo> getTodo(){
+        Todo todo= new Todo("Kutyaseta",false);
+        return ResponseEntity.status(HttpStatus.OK).body(todo);
+
     }
 }
